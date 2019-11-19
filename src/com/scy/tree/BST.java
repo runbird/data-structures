a0e3cf279a59dafec7b8330e620a15b7a9c98f26
SuoCaiyuan <cysuo@cupdata.com>
2019-11-19 21:02:41 +0800

    first commit by suocaiyuan

--- src/com/scy/tree/BST.java
+++ src/com/scy/tree/BST.java
@@ -1,5 +1,9 @@
 package com.scy.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * 类名： BST <br>
  * 描述：TODO <br>
@@ -96,14 +100,89 @@ public class BST<E extends Comparable<E>> {
     }
 
     //后序遍历
-    public void afterOrder() {
-        afterOrder(root);
+    public void postOrder() {
+        postOrder(root);
     }
 
-    private void afterOrder(Node node) {
+    private void postOrder(Node node) {
         if (node == null) return;
-        afterOrder(node.left);
-        afterOrder(node.right);
+        postOrder(node.left);
+        postOrder(node.right);
         System.out.println(node.e);
+    }
+
+    //前序遍历非递归
+    public void preOrderNR() {
+        preOrderNR(root);
+    }
+
+    private void preOrderNR(Node node) {
+        if (node == null) return;
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+    }
+
+    public void levelOrder() {
+        levelOrder(root);
+    }
+
+    private void levelOrder(Node node) {
+        if (node ==null) return;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    //获取BST最小值
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("empty BST");
+        }
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    //删除最小元素
+    public E removeEle() {
+        E minimum = minimum();
+        removeEle(root);
+        return minimum;
+    }
+    //删除二分搜索树中最小值
+    //并返回删除之后的BST根
+    private Node removeEle(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+        node.left = removeEle(node.left);
+        return node;
     }
 }
